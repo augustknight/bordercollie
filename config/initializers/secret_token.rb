06254e0803b21bbd7ab3b828d6ace05634e1f75c
Joseph Wilson <github@augustknight.com>
2012-03-17 15:32:38 -0400

    Initial Commit - Fresh Rails Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bordercollie::Application.config.secret_token = '2a461e0ece71a1a37476e820cc36a6fe438e584d5372b8e79d4132c08d78e928ee6250de914b2cd8ded5822fcddb8a49e53cb91f2acda84b55c589b8d339dad2'
